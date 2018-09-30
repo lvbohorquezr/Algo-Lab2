1c22d5c60a7e6c83b55e26c577200f7f4a0f9b1c
Laura Bohórquez Ramírez <37988367+lvbohorquezr@users.noreply.github.com>
2018-09-29 23:44:37 -0500

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,66 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package fibonacci;
-
-import java.util.Scanner;
+import java.util.*;
 import java.math.*;
 import java.lang.*;
 
-/**
- *
- * @author LauraB
- */
 public class Fibonacci {
-
-    /**
-     * @param args the command line arguments
-     */
-    static int fibo(int n){ // empieza a mandar overflow despues de fibonacci de 46
-        if(n==0){
+    static int Fibo(int n){ 
+        if(n == 0){
             return 0;
         }
-        int a=0;
-        int b = 1;
+        int p = 0;
+        int q = 1;
         for(int i = 1;i<n;i++ ){
-            b=a+b;
-            a=b-a;
-            
-                    
-        }
-        return b;        
+            q = p + q;
+            p = q - p;        
+          }
+        return q;        
     }
-    static byte fibo2(byte n){  //empieza a mandar overflow despues de fibonacci de 11
-        if(n==0){
+    
+    static byte Fibo2(byte n){  
+        if(n == 0){
             return 0;
         }
-        byte a=0;
-        byte b = 1;
+        byte p = 0;
+        byte q = 1;
         for(byte i = 1;i<n;i++ ){
-            b=	(byte) (a+b);
-            a=(byte)(b-a);
-            
-                    
+            q =	(byte) (p + q);
+            p = (byte)(q - p);           
         }
-        return b;        
+        return q;        
     }
-    static short fibo3(short n){//empieza a mandar overflow despues de fibonacci de 23
+    
+    static short Fibo3(short n){
         if(n==0){
             return 0;
         }
-        short a=0;
-        short b = 1;
+        short l = 0;
+        short m = 1;
         for(short i = 1;i<n;i++ ){
-            b=	(short) (a+b);
-            a=(short)(b-a);
-            
-                    
-        }
-        return b;        
+            m =	(short) (l + m);
+            l = (short)(m - l);           
+         }
+        return m;        
     }
-    static long fibo4(long n){//empieza a mandar overflow despues de fibonacci de 92
+    
+    static long Fibo4(long n){
         if(n==0){
             return 0;
         }
@@ -69,36 +51,34 @@ public class Fibonacci {
         for(long i = 1;i<n;i++ ){
             b=(a+b);
             a=(b-a);
-            
-                    
         }
         return b;        
     }
-    static BigInteger fibo5(BigInteger n){//no hay
+    
+    static BigInteger Fibo5(BigInteger n){
         if(n.equals(BigInteger.ZERO)){
             return BigInteger.ZERO;
         }
         BigInteger a=BigInteger.ZERO;
         BigInteger b = BigInteger.ONE;
-        for(BigInteger i = BigInteger.ONE;i.compareTo(n)<0;i=i.add(BigInteger.ONE) ){
-            b=	a.add(b);
-            a=b.subtract(a);
-            
-                    
-        }
+        for(BigInteger i = BigInteger.ONE;i.compareTo(n) < 0;i=i.add(BigInteger.ONE) ){
+            b =	a.add(b);
+            a = b.subtract(a);
+         }
         return b;        
     }
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        byte a=sc.nextByte();
-        short b = sc.nextShort();
-        int c = sc.nextInt();
-        long d = sc.nextLong();
-        BigInteger e = sc.nextBigInteger();
-        
-                
-        
-        System.out.println(fibo(c)+","+fibo2(a)+","+fibo3(b)+","+fibo4(d)+","+fibo5(e));
+        System.out.println("Input byte, short, int, long and BigInteger:");
+        Scanner scan = new Scanner(System.in);
+        byte a = scan.nextByte(); // overflow después de fibonacci de 46
+        short b = scan.nextShort(); //overflow después de fibonacci de 11
+        int c = scan.nextInt(); //overflow después de fibonacci de 23
+        long d = scan.nextLong(); //overflow después de fibonacci de 92
+        BigInteger e = scan.nextBigInteger();  //no hay
+        System.out.println("Fibonacci:");
+        System.out.println("byte [" + Fibo(c) + "], " + "short [" +Fibo2(a) + "], " + "int ["+ Fibo3(b) + "], " + 
+                "long [" + Fibo4(d) + "], " + "BigInteger [" + Fibo5(e) + "]");
         //System.out.println(fibo5(e)); //descomentar si quiere saber solo el fibonacci del BI
     }
     
