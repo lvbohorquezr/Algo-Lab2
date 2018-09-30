abb73d6e7f66e0312d1ba5efb739206724749d96
Laura Bohórquez Ramírez <37988367+lvbohorquezr@users.noreply.github.com>
2018-09-30 01:14:16 -0500

    Update and rename fibonaccip.py to FibonacciPython.py

--- FibonacciPython.py
+++ FibonacciPython.py
@@ -2,9 +2,6 @@ import time
 import numpy as np
 import pylab as pl
 
-
-
-
 def fibo_iterado(n):
     if n== 0:
         return 0
@@ -17,13 +14,13 @@ def fibo_iterado(n):
 
 lista_fib=[]
 lista_fibr=[]
-dato= raw_input("ingrese el numero maximo de fibonacci que desea calcular: ")
+dato= raw_input("Input the max number you want by Fibonacci: ")
 t0 = time.clock()
 for i in range (1, int(dato)+1):
     k = fibo_iterado(i)
     tf = time.clock()
     lista_fib.append(tf-t0)
-    print "el tiempo estimado para",i ," es de: ",tf-t0, " y el resultado es :", k
+    print "estimated time for ",i ," if: ",tf-t0, " and the ans is: ", k
 
 
 rango = np.arange(1,int(dato)+1,1)
